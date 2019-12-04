a4d175a8800470c35832d74ef1623efff17c02fe
Daniele <dev.daniele.calanna@gmail.com>
2019-12-04 17:08:43 +0100

    Load custom parameters for dog classification only

--- server/DogBreedClassifier.py
+++ server/DogBreedClassifier.py
@@ -6,6 +6,8 @@ import torch
 import torchvision.models as models
 import torchvision.transforms as transforms
 
+from torch.nn import Linear
+
 class DogBreedClassifier():
 
     def __init__(self, labels_path):
@@ -15,7 +17,9 @@ class DogBreedClassifier():
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
-        self.model = models.resnet50(pretrained=True)
+        self.model = models.resnet50(pretrained=False)
+        self.model.fc = Linear(in_features=2048, out_features=118, bias=True)
+        self.model.load_state_dict(torch.load("parameters/dog_classification_parameters.pth"))
         self.model.eval()
 
     def classify(self, img):
@@ -25,7 +29,7 @@ class DogBreedClassifier():
             out_sort = out.sort(descending=True)
             result = list(zip(out_sort[1][:10].tolist(), out_sort[0][:10].tolist()))
             for idx, probability in result:
-                print("{:.2f}%  {}".format(probability*100, self.labels[idx]))
+                print("{:.2f}%  {}".format(probability*100, self.labels[idx+151]))
             return result
 
 if __name__ == "__main__":
